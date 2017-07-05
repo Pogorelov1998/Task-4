ee4f4b47ce8dd2b08d4c11332ef64b41635971c7
Bogdan Pogorelov <boga-rap@mail.ru>
2017-07-05 16:37:18 +0500

    Task 4

---
+++ Program4.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Z4P
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите систему счисления: ");
+            int s = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите число: ");
+            string p = Console.ReadLine();
+            for(int i=0;i<p.Length;i++)
+                if (p[i] - '0' >= s)
+                {
+                    Console.Write("Число некорректно!\n");
+                    Console.ReadKey();
+                    return;
+                }
+            Console.Write("Число корректно!\n");
+            Console.ReadKey();
+        }
+    }
+}
